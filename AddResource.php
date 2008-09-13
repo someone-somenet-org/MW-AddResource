0ef5002f3ce2e55aa8b0a45d3d7bfd9c9a103a65
Mathias Ertl <mati@fsinf.at>
2008-09-13 18:33:23 +0000

    AddResource is now version 1.5.1. * The tabs now use getPrefixedDBkey to build the link-target * some coderestructuring:

--- AddResource.php
+++ AddResource.php
@@ -2,14 +2,19 @@
 # Not a valid entry point, skip unless MEDIAWIKI is defined
 if (!defined('MEDIAWIKI')) {
 	echo <<<EOT
-To install my extension, put the following line in LocalSettings.php:
+To install the AddResource extension, you must put at least the following
+code in LocalSettings.php:
 require_once( "$IP/extensions/AddResource/AddResource.php" );
+
+Note that several variables have to be set for this extension to become
+usefull. For full documentation please see:
+	http://pluto.htu.tuwien.ac.at/devel_wiki/AddResource
 EOT;
 	exit( 1 );
 }
 
+/* this is boilerplate (hook-registration etc.) */
 $dir = dirname(__FILE__);
-
 $wgAutoloadClasses['AddResource'] = dirname(__FILE__) . '/SpecialAddResource.php';
 $wgExtensionMessagesFiles['AddResource'] = $dir . '/AddResource.i18n.php';
 $wgSpecialPages[ 'AddResource' ] = 'AddResource';
@@ -19,11 +24,17 @@ $wgHooks['SkinTemplateContentActions'][] = 'efAddResourceDisplayTab';
 $wgExtensionCredits['specialpage'][] = array(
 	'name' => 'AddResource',
 	'description' => 'This special page allows you to \'\'\'attach\'\'\' resources to a given page',
-	'version' => '1.5-1.13.0',
+	'version' => '1.5.1-1.13.0',
 	'author' => 'Mathias Ertl',
 	'url' => 'http://pluto.htu.tuwien.ac.at/devel_wiki/AddResource',
 );
 
+/**
+ * These functions adds the localized pagename of the "Add resource" special-
+ * page.
+ * @param array $specialPageArray the current array of special pages
+ * @param unknown $code unknown.
+ */
 function efAddResourceLocalizedPageName( &$specialPageArray, $code) {
         wfLoadExtensionMessages('AddResource');
         $textMain = wfMsgForContent('addresource');
@@ -38,7 +49,15 @@ function efAddResourceLocalizedPageName( &$specialPageArray, $code) {
         return true;
 }
 
+/**
+ * This function is responsible for adding the tabs for the subject, talk and
+ * resources-page.
+ * 
+ * @param array tabs the original tabs
+ * @return boolean always true, there is no error-condition.
+ */
 function efAddResourceDisplayTab( $tabs ) {
+	/* some variables needed immediatly */
 	global $wgTitle, $wgAddResourceTab, $wgResourcesTabs;
 	$addResourcesTitle = SpecialPage::getTitleFor( 'AddResource' );
 	$curSpecialPage = $wgTitle->getPrefixedText();
@@ -48,33 +67,34 @@ function efAddResourceDisplayTab( $tabs ) {
 			! $wgAddResourceTab || ! $wgResourcesTabs )
 		return true;
 
+	/* get the requested page */
 	global $wgRequest, $wgUser;
-	$resourcesTitle = SpecialPage::getTitleFor( 'Resources' );
-	$reqTitle = Title::newFromText( $wgRequest->getVal('title') );
-        $par = preg_replace('/' . $curSpecialPage . '\/?/', '', $reqTitle->getPrefixedText() );
+	$par = ereg_replace( $wgTitle->getPrefixedDBkey() . '/?', '',
+		$wgRequest->getVal( 'title' ) );
 	if ( $par == '' ) // if no /par was given
 		return true;
-	$parTitle = Title::newFromText( $par )->getSubjectPage();
-	$parTalkTitle = $parTitle->getTalkPage();
 
-	/* build tabs */
+	/* build the subject page and add the tab */
 	$skin = $wgUser->getSkin();
+	$parTitle = Title::newFromText( $par )->getSubjectPage();
 	$nskey = $parTitle->getNamespaceKey();
-	
-	// subject page and talk page:
 	$customTabs[$nskey] = $skin->tabAction(
 		$parTitle, $nskey, false, '', true);
+
+	/* build the talk page and add the tab */
+	$parTalkTitle = $parTitle->getTalkPage();
 	$customTabs['talk'] = $skin->tabAction(
 		$parTalkTitle, 'talk', false, '', true);
-	
-	/* get number of resources */
+
+	// build subject-page tab:
+	$resourcesTitle = SpecialPage::getTitleFor( 'Resources' );
 	$resourcesPage = new Resources();
 	$resourcesCount = $resourcesPage->getResourceListCount( $parTitle );
 
 	$customTabs['view-resources'] = array ( 'class' => $resourcesCount ? false : 'new',
 		'text' => wfMsg('ResourcesTab'),
 		'href' => $resourcesTitle->getLocalURL() . '/' .
-			$parTitle->getPrefixedText()
+			$parTitle->getPrefixedDBkey()
 	);
 
 	$customTabs['add-resource'] = array ( 'class' => 'selected',
