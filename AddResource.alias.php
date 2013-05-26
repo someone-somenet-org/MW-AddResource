f22e402ea13b44bbcf729c888937bed3f3ec8f0c
Mathias Ertl <mati@fsinf.at>
2013-05-26 16:25:24 +0200

    add alias file

---
+++ AddResource.alias.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Aliases for AddResource
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+/** English
+ * @author Mathias Ertl
+ */
+$specialPageAliases['en'] = array(
+        'AddResource' => array('AddResource', 'Add Resource'),
+);
+
+/** Deutsch
+ * @author Mathias Ertl
+ */
+$specialPageAliases['de'] = array(
+        'AddResource' => array('Materialien_hinzufügen', 'Materialien hinzufügen'),
+);
